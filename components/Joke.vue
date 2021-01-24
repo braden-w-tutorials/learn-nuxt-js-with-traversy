0fffa751838cc74f27e617236b658035d624630a
Braden <bmw02002turbo@gmail.com>
2021-01-23 18:14:32 -0800

    Added joke

---
+++ components/Joke.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="joke">
+    <p>{{ joke }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Joke',
+  props: ['joke', 'id'],
+}
+</script>
+
+<style></style>
