c9fb6c381fef94445b4a3e28422bf2b1e893dd09
Braden <bmw02002turbo@gmail.com>
2021-01-23 20:12:30 -0800

    Console log

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -40,6 +40,7 @@ export default {
     try {
       const res = await axios.get('https://icanhazdadjoke.com/search', config)
       this.jokes = res.data.results
+      console.log(this.jokes)
     } catch (err) {
       console.error(err)
     }
