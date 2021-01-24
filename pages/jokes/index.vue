738d09832c8718cbdd3c6729a8dfe3ffe85c5b68
Braden <bmw02002turbo@gmail.com>
2021-01-23 18:13:24 -0800

    Async created

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -22,6 +22,18 @@ export default {
       ],
     }
   },
+  async created() {
+    const config = {
+      headers: { Accept: 'application/json' },
+    }
+
+    try {
+      const res = await axios.get('https://icanhazdadjoke.com/search', config)
+      console.log(res.data)
+    } catch (err) {
+      console.error(err)
+    }
+  },
   created() {},
 }
 </script>
