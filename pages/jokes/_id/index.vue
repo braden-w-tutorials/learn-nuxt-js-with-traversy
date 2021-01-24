e38c92a47d0650998c5af77e90f2d1627eb3b2b9
Braden <bmw02002turbo@gmail.com>
2021-01-23 20:24:33 -0800

    Added nuxt-link

--- pages/jokes/_id/index.vue
+++ pages/jokes/_id/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-  <div>{{ $route.params.id }}</div>
+    <nuxt-link to="/jokes"> Back To Jokes</nuxt-link>
     <h2>{{ joke }}</h2>
     <hr />
     <small>Joke ID: {{ $route.params.id }}</small>
