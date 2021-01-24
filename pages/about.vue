b576a93342c2cd3f57f1223441262b11fdb87033
Braden <bmw02002turbo@gmail.com>
2021-01-23 17:54:25 -0800

    Added SEO optimization

--- pages/about.vue
+++ pages/about.vue
@@ -6,7 +6,20 @@
 </template>
 
 <script>
-export default {}
+export default {
+  head() {
+    return {
+      title: 'About Us',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes',
+        },
+      ],
+    }
+  },
+}
 </script>
 
 <style></style>
