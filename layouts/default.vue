7e11c1f27f559990549ce8e2be89c5c18510e366
Braden <bmw02002turbo@gmail.com>
2021-01-23 18:07:50 -0800

    import AppHeader

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
+    <AppHeader />
     <Nuxt />
   </div>
 </template>
+
+<script>
+import AppHeader from '@/components/AppHeader'
+export default {
+  components: { AppHeader },
+}
+</script>
 
 <style>
 html {
