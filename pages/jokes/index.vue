94d89154c75bd5052f7f5e96a887ae05526be2a2
Braden <bmw02002turbo@gmail.com>
2021-01-23 18:09:12 -0800

    Added jokes index.vue

---
+++ pages/jokes/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Jokes List</h2>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
