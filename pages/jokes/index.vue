6c41b33f5bcff152a91f7359b138a77626dad62a
Braden <bmw02002turbo@gmail.com>
2021-01-23 17:58:48 -0800

    Move index vue

---
+++ pages/jokes/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Welcome to the best corny daad jokes app on the web!</h2>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'About Us',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
